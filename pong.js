9c6e95ce12ea9d8a7b68974ec34193973f3397d9
Mia Adjei <adjei.m@gmail.com>
2017-06-07 01:26:33 -0400

    Draw game board.

--- pong.js
+++ pong.js
@@ -1,1 +1,8 @@
-console.log('this is pong');
+window.onload = () => {
+	console.log('welcome to pong-ish! get ready to have some fun!');
+	const canvas = document.getElementById('gamespace');
+	const ctx = canvas.getContext('2d');
+
+	ctx.fillStyle = '#220a37';
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
