2c049e306693b6be9d84341b59c54accdbe71d45
Mia Adjei <adjei.m@gmail.com>
2017-06-07 23:45:00 -0400

    Add animation of square that moves across the page.

--- pong.js
+++ pong.js
@@ -1,8 +1,43 @@
+const animate = window.requestAnimationFrame;
+
+const canvas = document.createElement('canvas');
+const width = 700;
+const height = 500;
+
+canvas.width = width;
+canvas.height = height;
+
+const context = canvas.getContext('2d');
+
+let ball = 100;
+
+const step = () => {
+	update();
+	render();
+	animate(step);
+};
+
+const update = () => {
+
+};
+
+const render = () => {
+	ball = ball + 5;
+	console.log(ball);
+
+	context.fillStyle = '#220a37';
+	context.fillRect(0, 0, canvas.width, canvas.height);
+
+	context.fillStyle = 'orange';
+	context.fillRect(ball, 20, 20, 20);
+};
+
+
 window.onload = () => {
 	console.log('welcome to pong-ish! get ready to have some fun!');
-	const canvas = document.getElementById('gamespace');
-	const ctx = canvas.getContext('2d');
+	document.body.appendChild(canvas);
+	animate(step);
 
-	ctx.fillStyle = '#220a37';
-	ctx.fillRect(0, 0, canvas.width, canvas.height);
-}
+};
+
+
