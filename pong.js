a8b2a0aa7785ee0d2cd19a722cca94652a736502
Mia Adjei <adjei.m@gmail.com>
2017-06-08 00:50:41 -0400

    Render a paddle each for player and computer.

--- pong.js
+++ pong.js
@@ -9,8 +9,18 @@ canvas.height = height;
 
 const context = canvas.getContext('2d');
 
-let ball = 10;
+window.onload = () => {
+	console.log('welcome to pong-ish! get ready to have some fun!');
+	document.body.appendChild(canvas);
+	animate(step);
 
+};
+
+let ball = 10;
+const player = new Player();
+const computer = new Computer();
+
+// Update all objects, render them, then call animate again.
 const step = () => {
 	update();
 	render();
@@ -30,16 +40,38 @@ const render = () => {
 	context.fillStyle = '#220a37';
 	context.fillRect(0, 0, canvas.width, canvas.height);
 
-	context.fillStyle = 'orange';
+	context.fillStyle = '#ee7600';
 	context.fillRect(ball, 20, 20, 20);
+
+	player.paddle.render();
+	computer.paddle.render();
+};
+
+// Create the paddle.
+function Paddle(x, y, width, height) {
+	this.x = x;
+	this.y = y;
+	this.width = width;
+	this.height = height;
+
+	this.render = () => {
+		context.fillStyle = '#836fff';
+		context.fillRect(this.x, this.y, this.width, this.height);
+	};
+};
+
+// Make one paddle for the player and one for the computer.
+function Player() {
+	this.paddle = new Paddle(650, 225, 10, 70);
+	this.paddle.render();
+};
+
+function Computer() {
+	this.paddle = new Paddle(40, 225, 10, 70);
+	this.paddle.render();
 };
 
 
-window.onload = () => {
-	console.log('welcome to pong-ish! get ready to have some fun!');
-	document.body.appendChild(canvas);
-	animate(step);
 
-};
 
 
